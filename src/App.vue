637991175f2ce89040584e2285d4c5fe93cb4e2f
Anton <anton@chernianu.com>
2018-02-16 16:39:27 +0200

    parcel + vue �

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+	<div id="main-app">
+		<p>{{msgs}}</p>
+		<button v-on:click="clk">Click</button>
+		<p>{{count}}</p>
+		<!-- <test-app></test-app> -->
+	</div>
+</template>
+
+<script>
+// import Test from './test.vue';
+export default {
+	name: 'main-app',
+	data() {
+		return {
+			msgs: 'Components 😑',
+			count: 0
+		}
+	},
+	methods: {
+		clk : function(){
+			this.$data.count = this.$data.count + 1;
+		}
+	},
+	// components: {
+	// 	'test-app': Test
+	// }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/test.vue
@@ -0,0 +1,20 @@
+<template>
+	<div id="test">
+		<p>{{msg}}</p>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'Test',
+	data() {
+		return {
+			msg: '1. First'
+		}
+	}
+}
+</script>
+
+<style>
+
+</style>
